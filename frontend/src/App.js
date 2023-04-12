d71f2843b1f56a1a4b97682bd8c254f943c247bd
luismosca <luismosca@gmail.com>
2023-04-12 17:01:00 -0300

    Video-03-Create.React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
