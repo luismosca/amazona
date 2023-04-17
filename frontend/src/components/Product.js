933e7ef891b3be2316f721f833ea5f6e243cee90
luismosca <luismosca@gmail.com>
2023-04-17 20:45:25 -0300

    mas y mas

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,9 +2,30 @@ import Card from 'react-bootstrap/Card';
 import Buttom from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import axios from 'axios';
+import { useContext } from 'react';
+import { Store } from '../Store';
 
 function Product(props) {
   const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+  };
   return (
     <Card>
       <Link to={`/product/${product.slug}`}>
@@ -19,7 +40,13 @@ function Product(props) {
           numReviews={product.numReviews}
         ></Rating>
         <Card.Text>${product.price}</Card.Text>
-        <Buttom>Add to Cart</Buttom>
+        {product.countInStock === 0 ? (
+          <Buttom variant="ligth" disabled>
+            Out of Stock
+          </Buttom>
+        ) : (
+          <Buttom onClick={() => addToCartHandler(product)}>Add to Cart</Buttom>
+        )}
       </Card.Body>
     </Card>
   );
